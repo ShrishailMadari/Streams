4e62b09707f80f8fb3874934a8cd0d173065dbf1
shrishail madari <shrishailmadari@gmail.com>
2025-03-26 13:04:33 +0530

    add these files

--- streams/src/arrays/ReturnTheDuplicates.java
+++ streams/src/arrays/ReturnTheDuplicates.java
@@ -1,15 +1,20 @@
 package arrays;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class SumOfArrayElements {
+public class ReturnTheDuplicates {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 2, 1, 4, 4, 6, 9, 8, 9);
-        List<Integer> integerList = list.stream().distinct().toList();
+        List<Integer> integerList = new HashSet<>(list).stream().toList();
         System.out.println(integerList);
+        System.out.println("OR");
+        List<Integer> integers = list.stream().distinct().toList();
+        System.out.println(integers);
+
         Map<Integer, Long> collect = list.stream().collect(Collectors.groupingBy(n -> n, Collectors.counting()));
         System.out.println(collect);
         List<Integer> duplicates = collect.entrySet().stream().filter(num -> num.getValue() > 1)
